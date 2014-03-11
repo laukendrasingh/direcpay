63acddef8c406f5640f909893f34c52136da8416
laukendra <laukendra@intelligrape.com>
2014-03-11 12:10:58 +0530

    hot fix

--- direcPay/grails-app/controllers/com/intelligrape/direcPay/DirecPayController.groovy
+++ direcPay/grails-app/controllers/com/intelligrape/direcPay/DirecPayController.groovy
@@ -14,7 +14,6 @@ class DirecPayController {
      * @return redirect on success or failure url
      */
     def index(PaymentRequestCommand command) {
-
         println("......................make payment.....,\nparams: ${params.dump()},\nresponse: ${response.dump()}, \nrequest: ${request.dump()}, \n command: ${command.dump()}")
 
         if (!command.validate()) {
